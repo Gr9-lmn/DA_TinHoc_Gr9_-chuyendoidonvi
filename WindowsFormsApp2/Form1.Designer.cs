974f0fc9542709cbdd6deb04a704467c98d2a859
Gr9-lmn <nvn.25.05.2004@gmail.com>
2024-11-07 19:48:12 +0700

    Add project files.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBoxFrom = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.listBoxCategories = new System.Windows.Forms.ListBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBoxTo = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.label1.Location = new System.Drawing.Point(324, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Chuyển Đổi Đơn Vị";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.label2.Location = new System.Drawing.Point(26, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Chọn Đơn Vị Đổi";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.label3.Location = new System.Drawing.Point(56, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Nhập Giá Trị";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(167, 146);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(121, 26);
+            this.txtInput.TabIndex = 4;
+            this.txtInput.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.label4.Location = new System.Drawing.Point(307, 149);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(160, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Chọn Đơn Vị Cần Đổi";
+            // 
+            // comboBoxFrom
+            // 
+            this.comboBoxFrom.FormattingEnabled = true;
+            this.comboBoxFrom.Location = new System.Drawing.Point(497, 148);
+            this.comboBoxFrom.Name = "comboBoxFrom";
+            this.comboBoxFrom.Size = new System.Drawing.Size(188, 28);
+            this.comboBoxFrom.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(86, 200);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 20);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Kết Quả";
+            // 
+            // listBoxCategories
+            // 
+            this.listBoxCategories.FormattingEnabled = true;
+            this.listBoxCategories.ItemHeight = 20;
+            this.listBoxCategories.Items.AddRange(new object[] {
+            "Chiều Dài",
+            "Khối Lượng",
+            "Nhiệt Độ"});
+            this.listBoxCategories.Location = new System.Drawing.Point(167, 44);
+            this.listBoxCategories.Name = "listBoxCategories";
+            this.listBoxCategories.Size = new System.Drawing.Size(120, 84);
+            this.listBoxCategories.TabIndex = 2;
+            this.listBoxCategories.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(167, 194);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(120, 26);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.label6.Location = new System.Drawing.Point(340, 200);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(127, 20);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Chọn Đơn Vị Đổi";
+            // 
+            // comboBoxTo
+            // 
+            this.comboBoxTo.FormattingEnabled = true;
+            this.comboBoxTo.Location = new System.Drawing.Point(497, 194);
+            this.comboBoxTo.Name = "comboBoxTo";
+            this.comboBoxTo.Size = new System.Drawing.Size(188, 28);
+            this.comboBoxTo.TabIndex = 10;
+            this.comboBoxTo.SelectedIndexChanged += new System.EventHandler(this.comboBoxTo_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.button1.Location = new System.Drawing.Point(708, 149);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 73);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Đổi";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBoxTo);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.listBoxCategories);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBoxFrom);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBoxFrom;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox listBoxCategories;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBoxTo;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Thêm các loại đơn vị vào ListBox
+            listBoxCategories.Items.Add("Chiều Dài");
+            listBoxCategories.Items.Add("Khối Lượng");
+            listBoxCategories.Items.Add("Nhiệt Độ");
+            // Đặt tên listbox là: listBoxCategories
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            // Giả sử bạn có một TextBox tên là textBox2
+            textBox2.Enabled = false;  // Ngừng tương tác với TextBox (không thể chọn hay sao chép)
+            textBox2.ReadOnly = true;
+
+        }
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Xóa các mục hiện có trong ComboBox
+            comboBoxFrom.Items.Clear();
+            comboBoxTo.Items.Clear();
+
+            // Cập nhật các ComboBox dựa vào lựa chọn từ ListBox
+            switch (listBoxCategories.SelectedItem.ToString())
+            {
+                case "Chiều Dài":
+                    string[] lengthUnits = { "Kilometers (km)", "Hectometers (hm)", "Decameters (dam)",
+                                             "Meters (m)", "Decimeters (dm)", "Centimeters (cm)", "Millimeters (mm)" };
+                    comboBoxFrom.Items.AddRange(lengthUnits);
+                    comboBoxTo.Items.AddRange(lengthUnits);
+                    break;
+
+                case "Khối Lượng":
+                    string[] weightUnits = { "Tấn", "Tạ", "Yến", "Kilograms (kg)",
+                                             "Hectograms (hg)", "Decagrams (dag)", "Grams (g)" };
+                    comboBoxFrom.Items.AddRange(weightUnits);
+                    comboBoxTo.Items.AddRange(weightUnits);
+                    break;
+
+                case "Nhiệt Độ":
+                    string[] temperatureUnits = { "Celsius (°C)", "Fahrenheit (°F)", "Kelvin (K)" };
+                    comboBoxFrom.Items.AddRange(temperatureUnits);
+                    comboBoxTo.Items.AddRange(temperatureUnits);
+                    break;
+            }
+            // Thiết lập giá trị mặc định cho các ComboBox
+            comboBoxFrom.SelectedIndex = 0;
+            comboBoxTo.SelectedIndex = 1;
+            // Đặt tên cho 2 comnobox là: comboBoxFrom và comboBoxTo
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private double ConvertUnit(double value, string fromUnit, string toUnit)
+        {
+            if (fromUnit == toUnit) return value;
+
+            switch (fromUnit + " to " + toUnit)
+            {
+                // chiều dài
+                // Km sang các đơn vị còn lại
+                case "Kilometers (km) to Hectometers (hm)":  return value * 10;
+                case "Kilometers (km) to Decameters (dam)":  return value * 100;
+                case "Kilometers (km) to Meters (m)"      :  return value * 1000;
+                case "Kilometers (km) to Decimeters (dm)" :  return value * 10000;
+                case "Kilometers (km) to Centimeters (cm)":  return value * 100000;
+                case "Kilometers (km) to Millimeters (mm)":  return value * 1000000;
+
+                // hm sang các đơn vị còn lại
+                case "Hectometers (hm) to Kilometers (Km)" : return value / 10;
+                case "Hectometers (hm) to Decameters (dam)": return value * 10;
+                case "Hectometers (hm) to Meters (m)"      : return value * 100;
+                case "Hectometers (hm) to Decimeters (dm)" : return value * 1000;
+                case "Hectometers (hm) to Centimeters (cm)": return value * 10000;
+                case "Hectometers (hm) to Millimeters (mm)": return value * 100000;
+
+                // dam sang các đơn vị còn lại
+                case "Decameters (dam) to Kilometers (Km)"  : return value / 100;
+                case "Decameters (dam) to Hectometers (hm)" : return value / 10;
+                case "Decameters (dam) to Meters (m)"       : return value * 10;
+                case "Decameters (dam) to Decimeters (dm)"  : return value * 100;
+                case "Decameters (dam) to Centimeters (cm)" : return value * 1000;
+                case "Decameters (dam) to Millimeters (mm)" : return value * 10000;
+
+                // m sang các đơn vị còn lại
+                case "Meters (m) to Kilometers (Km)"    : return value / 1000;
+                case "Meters (m) to Hectometers (hm)"   : return value / 100;
+                case "Meters (m) to Decameters (dam)"   : return value / 10;
+                case "Meters (m) to Decimeters (dm)"    : return value * 10;
+                case "Meters (m) to Centimeters (cm)"   : return value * 100;
+                case "Meters (m) to Millimeters (mm)"   : return value * 1000;
+
+                // dm sang các đơn vị còn lại
+                case "Decimeters (dm) to Kilometers (Km)"   : return value / 10000;
+                case "Decimeters (dm) to Hectometers (hm)"  : return value / 1000;
+                case "Decimeters (dm) to Decameters (dam)"  : return value / 100;
+                case "Decimeters (dm) to Meters (m)"        : return value / 10;
+                case "Decimeters (dm) to Centimeters (cm)"  : return value * 10;
+                case "Decimeters (dm) to Millimeters (mm)"  : return value * 100;
+
+                // cm sang các đơn vị còn lại
+                case "Centimeters (cm) to Kilometers (Km)"  : return value / 100000;
+                case "Centimeters (cm) to Hectometers (hm)" : return value / 10000;
+                case "Centimeters (cm)  to Decameters (dam)": return value / 1000;
+                case "Centimeters (cm) to Meters (m)"       : return value / 100;
+                case "Centimeters (cm) to Decimeters (dm)"  : return value / 10;
+                case "Centimeters (cm) to Millimeters (mm)" : return value * 10;
+
+                // mm sang các đơn vị còn lại
+                case "Millimeters (mm) to Kilometers (Km)"  : return value / 1000000;
+                case "Millimeters (mm) to Hectometers (hm)" : return value / 100000;
+                case "Millimeters (mm)  to Decameters (dam)": return value / 10000;
+                case "Millimeters (mm) to Meters (m)"       : return value / 1000;
+                case "Millimeters (mm) to Decimeters (dm)"  : return value / 100;
+                case "Millimeters (mm) to Centimeters (cm)" : return value / 10;
+
+                // khối lượng
+                // tấn sang các đơn vị còn lại
+                case "Tấn to Tạ"                : return value * 10;
+                case "Tấn to Yến"               : return value * 100;
+                case "Tấn to Kilograms (kg)"    : return value * 1000;
+                case "Tấn to Hectograms (hg)"   : return value * 10000;
+                case "Tấn to Decagrams (dag)"   : return value * 100000;
+                case "Tấn to Grams (g)"         : return value * 1000000;
+
+
+                // tạ sang các đơn vị còn lại
+                case "Tạ to Tấn"                : return value / 10;
+                case "Tạ to Yến"                : return value * 10;
+                case "Tạ to Kilograms (kg)"     : return value * 100;
+                case "Tạ to Hectograms (hg)"    : return value * 1000;
+                case "Tạ to Decagrams (dag)"    : return value * 10000;
+                case "Tạ to Grams (g)"          : return value * 100000;
+
+
+                // yến sáng các đơn vị còn lại
+                case "Yến to Tấn"               : return value / 100;
+                case "Yến to Tạ"                : return value / 10;
+                case "Yến to Kilograms (kg)"    : return value * 10;
+                case "Yến to Hectograms (hg)"   : return value * 100;
+                case "Yến to Decagrams (dag)"   : return value * 1000;
+                case "Yến to Grams (g)"         : return value * 10000;
+
+
+                // kg sang các đơn vị còn lại
+                case "Kilograms (kg) to Tấn"            : return value / 1000;
+                case "Kilograms (kg) to Tạ"             : return value / 100;
+                case "Kilograms (kg) to Yến"            : return value / 10;
+                case "Kilograms (kg) to Hectograms (hg)": return value * 10;
+                case "Kilograms (kg) to Decagrams (dag)": return value * 100;
+                case "Kilograms (kg) to Grams (g)"      : return value * 1000;
+
+                // hg sang các đơn vị còn lại
+                case "Hectograms (hg) to Tấn"               : return value / 10000;
+                case "Hectograms (hg) to Tạ"                : return value / 1000;
+                case "Hectograms (hg) to Yến"               : return value / 100;
+                case "Hectograms (hg) to Kilograms (kg)"    : return value / 10;
+                case "Hectograms (hg) to Decagrams (dag)"   : return value * 10;
+                case "Hectograms (hg) to Grams (g)"         : return value * 100;
+
+
+                // dag sang các đơn vị còn lại
+                case "Decagrams (dag) to Tấn"               : return value / 100000;
+                case "Decagrams (dag) to Tạ"                : return value / 10000;
+                case "Decagrams (dag) to Yến"               : return value / 1000;
+                case "Decagrams (dag) to Kilograms (kg)"    : return value / 100;
+                case "Decagrams (dag) to Hectograms (hg)"   : return value / 10;
+                case "Decagrams (dag) to Grams (g)"         : return value * 10;
+
+
+                // g sang các đơn vị còn lại
+                case "Grams (g) to Tấn"                     : return value / 1000000;
+                case "Grams (g) to Tạ"                      : return value / 100000;
+                case "Grams (g) to Yến"                     : return value / 10000;
+                case "Grams (g) to Kilograms (kg)"          : return value / 1000;
+                case "Grams (g) to Hectograms (hg)"         : return value / 100;
+                case "Grams (g) to Decagrams (dag)"         : return value / 10;
+
+
+
+                // nhiệt độ
+                // C sang còn lại
+                case "Celsius (°C) to Fahrenheit (°F)"  : return (value * 9 / 5) + 32;
+                case "Celsius (°C) to Kelvin (K)"       : return value + 273.15;
+
+                // F sang còn lại
+                case "Fahrenheit (°F) to Celsius (°C)"  : return (value - 32) * 5 / 9;
+                case "Fahrenheit (°F) to Kelvin (K)"    : return ((value - 32) * 5 / 9) + 273.15;
+
+
+                // K sang còn lại
+                case "Kelvin (K) to Celsius (°C)"       : return value - 273.15;
+                case "Kelvin (K) to Fahrenheit (°F)"    : return ((value - 273.15) * 9 / 5) + 32;
+
+
+                default: throw new InvalidOperationException("Conversion not supported.");
+            }
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Kiểm tra xem giá trị trong txtInput có hợp lệ không
+                if (string.IsNullOrEmpty(txtInput.Text))
+                {
+                    MessageBox.Show("Vui lòng nhập giá trị vào ô đầu vào.");
+                    return;
+                }
+
+                // Chuyển giá trị nhập vào thành kiểu double
+                double inputValue = double.Parse(txtInput.Text);
+
+                // Lấy đơn vị nguồn và đích từ ComboBox
+                string fromUnit = comboBoxFrom.SelectedItem.ToString();
+                string toUnit = comboBoxTo.SelectedItem.ToString();
+
+                // Gọi hàm ConvertUnit để chuyển đổi giá trị
+                double result = ConvertUnit(inputValue, fromUnit, toUnit);
+
+                // Hiển thị kết quả trong textBox2
+                textBox2.Text = result.ToString("F2");  // Hiển thị kết quả với 2 chữ số thập phân
+            }
+            catch (FormatException)
+            {
+                // Nếu giá trị nhập vào không hợp lệ, hiển thị thông báo lỗi
+                MessageBox.Show("Vui lòng nhập giá trị hợp lệ.");
+            }
+            catch (Exception ex)
+            {
+                // Xử lý các lỗi khác (nếu có)
+                MessageBox.Show($"Đã xảy ra lỗi: {ex.Message}");
+            }
+        }
+
+        private void comboBoxTo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
+    
+
